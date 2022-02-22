88298a11b9375e1649cd7b094873bb7c528066db
capmahin <mahinulhuque1996@gmail.com>
2022-02-23 05:08:19 +0600

    resolve

---
+++ country.js
@@ -0,0 +1,21 @@
+const loadCountries = () =>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data =>displayCountries(data));
+}
+loadCountries();
+
+const displayCountries = counties =>{
+  const  countiesDiv = document.getElementById('countries');
+  counties.forEach(country =>{
+      console.log(country);
+      const div = document.createElement('div');
+      div.classList.add('country')
+      const h3 = document.createElement('h3');
+      h3.innerText = country.name;
+      div.appendChild(h3);
+      const p = document.createElement('p');
+      p.innerText = country.capital;
+      div.appendChild(p);
+  })
+}
