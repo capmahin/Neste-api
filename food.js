2f69ddb3a44b3d498b28be664ac3185052d265b0
capmahin <mahinulhuque1996@gmail.com>
2022-02-23 18:13:12 +0600

    need to resolve again

--- food.js
+++ food.js
@@ -14,7 +14,8 @@ const searchFood = () =>{
        const div = document.createElement('div');
        div.classList.add('col');
        div.innerHTML=`
-       <div class ="class h-100">
+       
+       <div onclick="loadMealDetail(${meal.id})" class ="class h-100">
        <img src ="${meal.strMealThumb}" class = "card-img-top"
        alt="...">
        <div class ="card-body">
@@ -25,6 +26,29 @@ const searchFood = () =>{
        `;
        searchResult.appendChild(div);
      });
+   }
+
+   const loadMealDetail = mealId =>{
+     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+     fetch(url)
+     .then(res => res.json())
+     .then(data => displayMealDetail(data.meals[0]));
+   }
+
+   const displayMealDetail = meal =>{
+      console.log(meal);
+      const mealDetails = document.getElementById('meal-details');
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML = `
+      <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${meal.strMeal}</h5>
+    <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+  </div>
+  `;
+  mealDetails.appendChild(div);
    }
    
    
